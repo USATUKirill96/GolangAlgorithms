7204aa9932d81401b80cc3ba05a5e6b01cc3e452
Gorill <kirill.ivanov96@gmail.com>
2021-12-20 12:16:27 +0500

    104, 112

---
+++ 112.path_sum.go
@@ -0,0 +1,31 @@
+package main
+
+type treeNode struct {
+	Val   int
+	Left  *treeNode
+	Right *treeNode
+}
+
+func hasPathSum(root *treeNode, targetSum int) bool {
+	found := false
+	hasPathSumHelper(root, 0, targetSum, &found)
+	return found
+}
+
+func hasPathSumHelper(root *treeNode, prevSum, required int, found *bool) {
+	if root == nil {
+		return
+	}
+	if root.Left != nil {
+		hasPathSumHelper(root.Left, prevSum+root.Val, required, found)
+	}
+	if root.Right != nil {
+		hasPathSumHelper(root.Right, prevSum+root.Val, required, found)
+	}
+	if root.Left == nil && root.Right == nil {
+		if prevSum+root.Val == required {
+			*found = true
+		}
+	}
+
+}
