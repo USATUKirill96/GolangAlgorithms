01ffcb4d980c1aec2473d2a3ac99a36b5ba72254
Gorill <kirill.ivanov96@gmail.com>
2022-01-13 11:28:51 +0500

    Monotonic Array

--- 1945.SumOfDigitsAndStringAfterConvert_test.go
+++ 1945.SumOfDigitsAndStringAfterConvert_test.go
@@ -6,13 +6,13 @@ import (
 	"testing"
 )
 
-//You are given a string s consisting of lowercase English letters, and an integer k.
+//You are given a string input consisting of lowercase English letters, and an integer k.
 
-//First, convert s into an integer by replacing each letter with its position in the alphabet
+//First, convert input into an integer by replacing each letter with its position in the alphabet
 //(i.e., replace 'a' with 1, 'b' with 2, ..., 'z' with 26).
 // Then, transform the integer by replacing it with the sum of its digits. Repeat the transform operation k times in total.
 
-//For example, if s = "zbax" and k = 2, then the resulting integer would be 8 by the following operations:
+//For example, if input = "zbax" and k = 2, then the resulting integer would be 8 by the following operations:
 
 //Convert: "zbax" ➝ "(26)(2)(1)(24)" ➝ "262124" ➝ 262124
 //Transform #1: 262124 ➝ 2 + 6 + 2 + 1 + 2 + 4 ➝ 17
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module USATUKirill96/leetcode
 
 go 1.17
-
-require github.com/mkouhei/gosh v0.2.3 // indirect
